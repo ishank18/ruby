5737fb2638bc333ed66b5eda181da5c8d6f879d5
Ishank Gupta <ishank.gupta@vinsol.com>
2011-10-05 18:41:40 +0530

    Test

--- pascalYeild.rb
+++ pascalYeild.rb
@@ -1,5 +1,7 @@
 def pascal(x)
-	upper_layer = Array.new()
+	yield x			
+end
+pascal(6){|x| upper_layer = Array.new()
 	lower_layer = Array.new()
 	for i in 0..100
 		upper_layer[i] = 0
@@ -8,19 +10,14 @@ def pascal(x)
 	upper_layer[0] = 0
 	upper_layer[1] = 1
 	upper_layer[2] = 0
-	lower_layer[0] = 0
-	for i in 0..x+1
+	for i in 1..x+1
 		for j in 1..i
-			lower_layer[j] = upper_layer[j-1] + upper_layer[j]
-			print lower_layer[j].to_s + " "
-			upper_layer = lower_layer.dup
+			lower_layer[j] = upper_layer[j-1]+ upper_layer[j]
+			print lower_layer[j].to_s + " "					
 		end
-				
-		puts ""
-		#p upper_layer
-		#p lower_layer
-		
-	end		
-			
-end
-pascal(6)
+		if(i != 0)
+			upper_layer = lower_layer.dup	
+		end
+		puts ""		
+	end }
+	
